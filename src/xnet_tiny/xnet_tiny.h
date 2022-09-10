e5af3b3092da6b4a53383ce7ef00131a23bd4593
binarywz <binarywz@163.com>
2022-09-10 23:01:12 +0800

    以太网驱动封装

--- src/xnet_tiny/xnet_tiny.h
+++ src/xnet_tiny/xnet_tiny.h
@@ -5,6 +5,11 @@
 
 #define  XNET_CFG_PACKET_MAX_SIZE 1516 // 收发数据包的最大大小
 
+typedef enum _xnet_err_t {
+    XNET_ERR_OK = 0,
+    XNET_ERR_IO = -1
+} xnet_err_t;
+
 typedef struct _xnet_packet_t {
     uint16_t size;                              // 数据包有效数据的大小
     uint8_t* data;                              // 数据包数据的起始地址
@@ -16,5 +21,9 @@ xnet_packet_t* xnet_alloc_for_read(uint16_t size);
 
 void xnet_init(void);
 void xnet_poll(void);
+
+xnet_err_t xnet_driver_open(uint8_t* mac_addr);
+xnet_err_t xnet_driver_send(xnet_packet_t* packet);
+xnet_err_t xnet_driver_read(xnet_packet_t** packet);
 
 #endif // XNET_TINY_H
