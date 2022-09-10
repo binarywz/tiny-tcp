1561be06a1e0db1ceb73e2cece466458499ece01
binarywz <binarywz@163.com>
2022-09-10 22:36:42 +0800

    数据包结构定义

--- src/xnet_tiny/xnet_tiny.c
+++ src/xnet_tiny/xnet_tiny.c
@@ -1,1 +1,41 @@
 ﻿#include "xnet_tiny.h"
+
+#define min(a, b) ((a) > (b) ? (b) : (a))
+static xnet_packet_t tx_packet, rx_packet;
+
+xnet_packet_t* xnet_alloc_for_send(uint16_t size) {
+    tx_packet.data = tx_packet.payload + XNET_CFG_PACKET_MAX_SIZE - size;
+    tx_packet.size = size;
+    return &tx_packet;
+}
+
+xnet_packet_t* xnet_alloc_for_read(uint16_t size) {
+    rx_packet.data = rx_packet.payload;
+    rx_packet.size = size;
+    return &rx_packet;
+}
+
+void xnet_init(void) {
+
+}
+
+void xnet_poll(void) {
+
+}
+
+// 添加packet头部
+static void add_header(xnet_packet_t* packet, uint16_t size) {
+    packet->data -= size;
+    packet->size += size;
+}
+
+// 移除packet头部
+static void remove_header(xnet_packet_t* packet, uint16_t size) {
+    packet->data += size;
+    packet->size -= size;
+}
+
+// 截断packet
+static void truncate_packet(xnet_packet_t* packet, uint16_t size) {
+    packet->size = min(packet->size, size);
+}
--- src/xnet_tiny/xnet_tiny.h
+++ src/xnet_tiny/xnet_tiny.h
@@ -1,5 +1,20 @@
 ﻿#ifndef XNET_TINY_H
 #define XNET_TINY_H
 
-#endif // XNET_TINY_H
+#include <stdint.h>
 
+#define  XNET_CFG_PACKET_MAX_SIZE 1516 // 收发数据包的最大大小
+
+typedef struct _xnet_packet_t {
+    uint16_t size;                              // 数据包有效数据的大小
+    uint8_t* data;                              // 数据包数据的起始地址
+    uint8_t payload[XNET_CFG_PACKET_MAX_SIZE];  // 负载数组
+} xnet_packet_t;
+
+xnet_packet_t* xnet_alloc_for_send(uint16_t size);
+xnet_packet_t* xnet_alloc_for_read(uint16_t size);
+
+void xnet_init(void);
+void xnet_poll(void);
+
+#endif // XNET_TINY_H
